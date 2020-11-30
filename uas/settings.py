a9db7bfa0527fe81b9e38f6771c23f877005e342
fernandyoung <75234850+fernandyoung@users.noreply.github.com>
2020-11-30 12:29:04 +0700

    Update settings.py

--- uas/settings.py
+++ uas/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = BASE_DIR, 'static'
